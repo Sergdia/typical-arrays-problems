50630fd357c961d4e436af09adb1d423b820b60c
Sergei <sdiagil@gmail.com>
2020-10-02 17:34:56 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 
 exports.min = function min (array) {
-    array.sort((a, b) => (a - b));
-    return array[0];
-}
+    if (array == undefined) {
+        return 0;
+      } else if (array.length > 0) {
+      array.sort((a, b) => (a - b));
+      return array[0];
+      } else {
+         return 0;
+      }
+    }
+
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (array == undefined) {
+        return 0;
+      } else if (array.length > 0) {
+      array.sort((a, b) => (a - b));
+      return array[(array.length - 1)];
+      } else {
+         return 0;
+      }
+    }
+
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined) {
+      return 0;
+      } else if (array.length > 0) {
+        var count = 0;
+        for(var i = 0; i < array.length; i++) {
+      
+          count = count + array[i];
+          
+      } return count/array.length;
+      } else { return 0; }
 }
